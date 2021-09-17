9a0008f4234f2a8e5dd412c63619d4fa845b30b0
Morgan Hallgren <morganh@axis.com>
2021-09-17 11:49:44 +0200

    fix html styles

--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,17 @@ import (
 )
 
 func root(w http.ResponseWriter, req *http.Request) {
-	h := hypermedia.Hypermedia{}
+	h := hypermedia.New()
 	h.AddLink("self", "/", "self")
 	h.AddLink("devices", "/device", "devices")
 	h.AddLink("external", "http://google.com", "google")
+	h.AddProperty("test", "value")
+	h.AddProperty("morgan", "hallgren")
 	hypermedia.RenderHTML(w, h)
 }
 
 func devices(w http.ResponseWriter, req *http.Request) {
-	h := hypermedia.Hypermedia{}
+	h := hypermedia.New()
 	h.AddLink("self", "/device", "devices")
 	h.AddLink("device", "/device/1", "device 1")
 	h.AddLink("device", "/device/2", "device 2")
@@ -28,8 +30,9 @@ func device(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	deviceURL := "/device/" + vars["id"]
 	deviceName := "device " + vars["id"]
-	h := hypermedia.Hypermedia{}
+	h := hypermedia.New()
 
+	h.AddProperty("id", vars["id"])
 	h.AddLink("self", deviceURL, deviceName)
 	h.AddLink("devices", "/device", "devices")
 	h.AddLink("root", "/", "root")
--- html.go
+++ html.go
@@ -10,14 +10,130 @@ const tpl = `
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style type="text/css">
+      * {
+        box-sizing: border-box;
+      }
+
+      body {
+        background-color: #F9E9DE;
+        margin: 0;
+        font-family: sans-serif;
+      }
+
+      .resource {
+        margin-left: 0.3em;
+        padding-left: 0.3em;
+      }
+
+      body .resource {
+        padding-right: 0.5em;
+      }
+
+      .resource .resource {
+        border-left: 4px solid #bababa;
+      }
+
+      h1,h2,h3,h4,h5 {
+        background-color: #FFB662;
+        padding: 0.2em;
+        margin-bottom: 0.2em;
+      }
+
+      .attributes {
+        margin-bottom: 0.2em;
+      }
+
+      th { background-color: #FFD19D; }
+      td { background-color: #E2ECBA; }
+
+      .rel a {
+        color: black;
+        text-decoration: none;
+      }
+
+      .subresource {
+        padding-top: 1em;
+      }
+
+      .subresource-rel {
+        background-color: #FFB662;
+        padding: 0.5em;
+        margin-bottom: 0.5em;
+      }
+
+      .header, .footer {
+        width: 100%;
+        background-color: black;
+        color: white;
+        padding: 0.3em;
+      }
+
+      .footer a {
+        text-decoration: none;
+        color: white;
+      }
+
+      .footer {
+        margin-top: 1em;
+      }
+
+      .collapsed .heading::before, .expanded .heading::before {
+        font-size: 0.6em;
+        display: block;
+        float: left;
+        margin-top: 0.4em;
+        margin-right: 0.5em;
+      }
+
+      .collapsed .heading::before {
+        content: "▶";
+      }
+
+      .expanded .heading::before {
+        content: "▼";
+      }
+
+      .collapsed .body {
+        display: none;
+      }
+    </style>
 	</head>
 	<body>
-		{{range .Links}}<a rel="{{.Rel}}" href="{{.URL}}">{{ .Name }}</a><br />{{end}}
+		<div class="header request-info">
+		</div>
+		<div class="resource">
+		<h1 class="heading type">Resource Type</h1>
+		<div class="resource-data body">
+
+		<table class="attributes">
+		{{range $key, $value := .Properties}}
+			<tr class="attribute"><td class="name">{{ $key }}</td><td class="value">{{ $value }}</td></tr>
+		{{end}}
+		</table>
+
+		<table class="links">
+			<tr>
+				<th>Rel</th>
+				<th>URI</th>
+				<th>Title</th>
+				<th>Templated</th>
+			</tr>
+			{{range .Links}}
+			<tr class="link">
+				<td class="rel"><a href="">{{ .Rel }}</a></td>
+				<td class="uri"><a href="{{ .URL }}">{{ .URL }}</a></td>
+				<td class="title">{{ .Name }}</td>
+				<td class="templated">false</td>
+			</tr>
+			{{end}}
+		  </table}
 	</body>
 </html>`
 
 var t, _ = template.New("webpage").Parse(tpl)
 
-func RenderHTML(w io.Writer, h Hypermedia) error {
+func RenderHTML(w io.Writer, h *Hypermedia) error {
 	return t.Execute(w, h)
 }
