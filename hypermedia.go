0eeb4b0d88e34f897d09e8b87210c380eec9ef78
Morgan Hallgren <morganh@axis.com>
2021-09-17 11:49:25 +0200

    added properties

--- hypermedia.go
+++ hypermedia.go
@@ -7,11 +7,23 @@ type Link struct {
 }
 
 type Hypermedia struct {
-	Links []Link
+	Links      []Link
+	Properties map[string]string
+}
+
+func New() *Hypermedia {
+	return &Hypermedia{
+		Links:      make([]Link, 0, 0),
+		Properties: make(map[string]string),
+	}
 }
 
 // AddLink adds a link to the hypermedia struct
 func (h *Hypermedia) AddLink(rel, url, name string) {
 	l := Link{Rel: rel, URL: url, Name: name}
 	h.Links = append(h.Links, l)
+}
+
+func (h *Hypermedia) AddProperty(key, value string) {
+	h.Properties[key] = value
 }
