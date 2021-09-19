e1b462381d5e37589cead9a2a312f9f2546da199
Morgan Hallgren <morgan.hallgren@gmail.com>
2021-09-19 22:40:04 +0200

    redo example

--- cmd/main.go
+++ cmd/main.go
@@ -7,51 +7,72 @@ import (
 	"github.com/hallgren/hypermedia"
 )
 
+type Item struct {
+	ID string
+}
+
+var Items = []Item{}
+
 func root(w http.ResponseWriter, req *http.Request) {
 	h := hypermedia.New("root")
 	h.AddLink("self", "/", "self")
-	h.AddLink("devices", "/device", "devices")
-	h.AddLink("external", "http://google.com", "google")
-	h.AddProperty("root", "root")
-	r := h.AddResource("morgan")
-	r.AddLink("morgan", "/morgan", "morgan")
-	r.AddProperty("morgan", "morgan")
-	s := h.AddResource("martin")
-	s.AddLink("martin", "/martin", "martin")
-	s.AddProperty("martin", "martin")
+	h.AddLink("items", "/items", "items")
 	hypermedia.RenderHTML(w, h)
 }
 
-func devices(w http.ResponseWriter, req *http.Request) {
-	h := hypermedia.New("devices")
-	h.AddLink("self", "/device", "devices")
-	h.AddLink("device", "/device/1", "device 1")
-	h.AddLink("device", "/device/2", "device 2")
-	h.AddLink("root", "/", "root")
-	h.AddProperty("count", "2")
-	f := h.AddForm("/devices", "POST")
-	f.AddInput("123", "text", "name", "123", "username")
-	f.AddInput("1234", "password", "name", "", "")
-	hypermedia.RenderHTML(w, h)
+func items(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		h := hypermedia.New("items")
+		h.AddLink("self", "items", "items")
+
+		for _, i := range Items {
+			r := h.AddResource(i.ID)
+			r.AddProperty("id", i.ID)
+			r.AddLink("item", "/items/"+i.ID, "item")
+			f := r.AddForm("/items/"+i.ID, "POST")
+			f.AddInput("delete", "submit", "Delete", "Delete", "")
+		}
+
+		// create item form
+		f := h.AddForm("items", "POST")
+		f.AddInput("create", "text", "create", "", "ID")
+		f.AddInput("create", "submit", "submit", "Create", "")
+		hypermedia.RenderHTML(w, h)
+	} else if req.Method == "POST" {
+		req.ParseForm()
+		s := req.FormValue("create")
+		Items = append(Items, Item{ID: s})
+		http.Redirect(w, req, "/items/"+s, http.StatusSeeOther)
+	}
 }
 
-func device(w http.ResponseWriter, req *http.Request) {
+func item(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
-	deviceURL := "/device/" + vars["id"]
-	deviceName := "device " + vars["id"]
-	h := hypermedia.New("device")
+	if req.Method == "GET" {
+		url := "/items/" + vars["id"]
+		h := hypermedia.New("item")
 
-	h.AddProperty("id", vars["id"])
-	h.AddLink("self", deviceURL, deviceName)
-	h.AddLink("devices", "/device", "devices")
-	h.AddLink("root", "/", "root")
-	hypermedia.RenderHTML(w, h)
+		h.AddProperty("id", vars["id"])
+		h.AddLink("self", url, "item")
+		h.AddLink("items", "/items", "items")
+		h.AddLink("root", "/", "root")
+		f := h.AddForm("/items/"+vars["id"], "POST")
+		f.AddInput("delete", "submit", "Delete", "Delete", "")
+		hypermedia.RenderHTML(w, h)
+	} else if req.Method == "POST" {
+		for s, i := range Items {
+			if i.ID == vars["id"] {
+				Items = append(Items[:s], Items[s+1:]...)
+			}
+		}
+		http.Redirect(w, req, "/items", http.StatusSeeOther)
+	}
 }
 
 func main() {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", root)
-	mux.HandleFunc("/device", devices)
-	mux.HandleFunc("/device/{id:[1-9]+}", device)
+	mux.HandleFunc("/items", items)
+	mux.HandleFunc("/items/{id:[1-9]+}", item)
 	http.ListenAndServe(":8090", mux)
 }
